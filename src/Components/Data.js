f2b247641ee8701757e01ea462451e99563b8113
hvjee <123385008+hvjee@users.noreply.github.com>
2023-07-23 20:01:50 +0600

    Update Data.js

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -43,7 +43,7 @@ const Items = [
 		id: 2,
 		itemId: "buger01",
 		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/ules-e5ea3.appspot.com/o/%D1%88%D0%B0%D1%88%D0%BB%D1%8B%D0%BA.png?alt=media&token=ed03aab6-7e4e-4cef-b37e-8699a56d47e8",
+			"https://firebasestorage.googleapis.com/v0/b/ules-e5ea3.appspot.com/o/%D1%84%D0%B0%D1%80%D1%88.png?alt=media&token=547eeeca-aba6-4b03-9287-600e17ed4320",
 		name: "Фарш",
 		ratings: 500,
 		price: "500",
@@ -52,7 +52,7 @@ const Items = [
 		id: 3,
 		itemId: "buger01",
 		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/ules-e5ea3.appspot.com/o/%D1%88%D0%B0%D1%88%D0%BB%D1%8B%D0%BA.png?alt=media&token=ed03aab6-7e4e-4cef-b37e-8699a56d47e8",
+			"https://firebasestorage.googleapis.com/v0/b/ules-e5ea3.appspot.com/o/%D0%BA%D1%83%D1%80%D0%B8%D0%BD%D1%8B%D0%B9.png?alt=media&token=a0d9ddc0-3b87-41d5-b25c-0d221250ddd9",
 		name: "Тауық",
 		ratings: 750,
 		price: "750",
@@ -61,7 +61,7 @@ const Items = [
 		id: 4,
 		itemId: "buger01",
 		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/ules-e5ea3.appspot.com/o/%D1%88%D0%B0%D1%88%D0%BB%D1%8B%D0%BA.png?alt=media&token=ed03aab6-7e4e-4cef-b37e-8699a56d47e8",
+			"https://firebasestorage.googleapis.com/v0/b/ules-e5ea3.appspot.com/o/%D1%80%D0%B5%D0%B1%D1%80%D0%B0.png?alt=media&token=11e4bde3-9231-44bf-aa54-f09b1310f3b8",
 		name: "Қабырға",
 		ratings: 950,
 		price: "950",
@@ -70,7 +70,7 @@ const Items = [
 		id: 5,
 		itemId: "buger01",
 		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/ules-e5ea3.appspot.com/o/%D1%88%D0%B0%D1%88%D0%BB%D1%8B%D0%BA.png?alt=media&token=ed03aab6-7e4e-4cef-b37e-8699a56d47e8",
+			"https://firebasestorage.googleapis.com/v0/b/ules-e5ea3.appspot.com/o/%D0%B1%D0%B0%D1%83%D1%8B%D1%80.png?alt=media&token=e89ca806-ff6d-4a3c-97b2-4642c3e945cb",
 		name: "Бауыр",
 		ratings: 500,
 		price: "500",
@@ -79,7 +79,7 @@ const Items = [
 		id: 6,
 		itemId: "buger01",
 		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/ules-e5ea3.appspot.com/o/%D1%88%D0%B0%D1%88%D0%BB%D1%8B%D0%BA.png?alt=media&token=ed03aab6-7e4e-4cef-b37e-8699a56d47e8",
+			"https://firebasestorage.googleapis.com/v0/b/ules-e5ea3.appspot.com/o/%D1%80%D1%8B%D0%B1%D0%B0.png?alt=media&token=06fb7951-30d1-4194-acfb-486e46c85b13",
 		name: "Балық",
 		ratings: 500,
 		price: "500",
@@ -211,320 +211,42 @@ const Items = [
 		price: "700",
 	},
 	{
-		id: 19,
-		itemId: "taco01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Ftaco1.png?alt=media&token=b78c8e68-c1a6-4e43-8c43-6ef9f23aa59f",
-		name: "Taco",
-		ratings: 3,
-		price: "7.5",
-	},
-	{
-		id: 20,
-		itemId: "taco01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Ftaco2.png?alt=media&token=a6e8d72e-f288-48dc-9f26-8322774f5216",
-		name: "Taco",
-		ratings: 3,
-		price: "7.5",
-	},
-	{
-		id: 21,
-		itemId: "taco01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Ftaco3.png?alt=media&token=daa2cf5b-36b7-426a-9f2c-73ae70e62634",
-		name: "Taco",
-		ratings: 3,
-		price: "7.5",
-	},
-	{
-		id: 22,
-		itemId: "taco01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Ftaco4.png?alt=media&token=5d067124-870f-4ea8-afe1-5bd4f94ca20c",
-		name: "Taco",
-		ratings: 3,
-		price: "9",
-	},
-	{
-		id: 23,
-		itemId: "taco01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Ftaco5.png?alt=media&token=910f236f-2149-46cf-9b74-7d1ee0323c93",
-		name: "Taco",
-		ratings: 3,
-		price: "9",
-	},
-	{
-		id: 24,
-		itemId: "taco01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Ftaco6.png?alt=media&token=91214a30-cad0-434d-9339-3c658150ccab",
-		name: "Taco",
-		ratings: 3,
-		price: "9",
-	},
-	{
-		id: 25,
-		itemId: "snack01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fsnack1.png?alt=media&token=d670617e-4bfc-4693-aa8c-211db9f9d706",
-		name: "Snack",
-		ratings: 4,
-		price: "9",
-	},
-	{
-		id: 26,
-		itemId: "snack01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fsnack2.png?alt=media&token=55d7fc65-869b-4d8b-83b3-be0158474413",
-		name: "Snack",
-		ratings: 4,
-		price: "9",
-	},
-	{
-		id: 27,
-		itemId: "snack01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fsnack3.png?alt=media&token=76b488a6-be44-4a3e-a29e-c34ac17f2e2c",
-		name: "Snack",
-		ratings: 4,
-		price: "9",
-	},
-	{
-		id: 28,
-		itemId: "snack01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fsnack4.png?alt=media&token=1efcd8c0-098b-44a5-8113-da225e7358ce",
-		name: "Snack",
-		ratings: 4,
-		price: "9",
-	},
-	{
-		id: 29,
-		itemId: "snack01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fsnack5.png?alt=media&token=80cece50-38cd-489f-a73f-edd740fba4ab",
-		name: "Snack",
-		ratings: 4,
-		price: "9",
-	},
-	{
-		id: 30,
-		itemId: "snack01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fsnack6.png?alt=media&token=a36cb2c5-72d1-4364-9817-3a8eb8c244b6",
-		name: "Snack",
-		ratings: 4,
-		price: "9",
-	},
-	{
 		id: 31,
 		itemId: "drink01",
 		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdrink1.png?alt=media&token=e9cc5e4e-2194-4766-9d5a-5e01657927b7",
-		name: "Soft Drinks",
+			"https://firebasestorage.googleapis.com/v0/b/ules-e5ea3.appspot.com/o/%D0%BA%D0%BE%D0%BB%D0%B0.png?alt=media&token=fed80c82-18fe-4bcd-a861-bc65980db5d7",
+		name: "Coca Cola",
 		ratings: 4,
-		price: "9",
+		price: "450",
 	},
 	{
 		id: 32,
 		itemId: "drink01",
 		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdrink2.png?alt=media&token=872c5ddd-23ae-4f1e-b1dd-18d375240ba6",
-		name: "Soft Drinks",
+			"https://firebasestorage.googleapis.com/v0/b/ules-e5ea3.appspot.com/o/%D0%BF%D0%B5%D0%BF%D1%81%D0%B8.png?alt=media&token=f7d5000f-a3ff-4082-83ab-fa1dc37e4c0a",
+		name: "Pepsi ",
 		ratings: 4,
-		price: "7.5",
+		price: "450",
 	},
 	{
 		id: 33,
 		itemId: "drink01",
 		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdrink3.png?alt=media&token=c0efdccf-149a-4063-9658-00ae6c7c69dc",
-		name: "Soft Drinks",
+			"https://firebasestorage.googleapis.com/v0/b/ules-e5ea3.appspot.com/o/%D1%84%D0%B0%D0%BD%D1%82%D0%B0.png?alt=media&token=a15e9cc2-c9d7-4964-b40a-efade3b0114e",
+		name: "Fanta",
 		ratings: 4,
-		price: "7.5",
+		price: "450",
 	},
 	{
 		id: 34,
 		itemId: "drink01",
 		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdrink4.png?alt=media&token=eaaa8826-7fc0-499f-9eaf-8369ff99c112",
-		name: "Soft Drinks",
+			"https://firebasestorage.googleapis.com/v0/b/ules-e5ea3.appspot.com/o/%D1%81%D0%BF%D1%80%D0%B0%D0%B9%D1%82.png?alt=media&token=7ee292f4-ca11-4621-96f2-cd0ea233af43",
+		name: "Sprite",
 		ratings: 4,
-		price: "7.5",
-	},
-	{
-		id: 35,
-		itemId: "drink01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdrink5.png?alt=media&token=eecf1eee-73f5-4705-a7ce-9ccc363f4f47",
-		name: "Soft Drinks",
-		ratings: 4,
-		price: "7.5",
-	},
-	{
-		id: 59,
-		itemId: "taco01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Ftaco1.png?alt=media&token=b78c8e68-c1a6-4e43-8c43-6ef9f23aa59f",
-		name: "Taco",
-		ratings: 3,
-		price: "7.5",
-	},
-	{
-		id: 60,
-		itemId: "taco01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Ftaco2.png?alt=media&token=a6e8d72e-f288-48dc-9f26-8322774f5216",
-		name: "Taco",
-		ratings: 3,
-		price: "7.5",
-	},
-	{
-		id: 61,
-		itemId: "taco01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Ftaco3.png?alt=media&token=daa2cf5b-36b7-426a-9f2c-73ae70e62634",
-		name: "Taco",
-		ratings: 3,
-		price: "7.5",
-	},
-	{
-		id: 62,
-		itemId: "taco01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Ftaco4.png?alt=media&token=5d067124-870f-4ea8-afe1-5bd4f94ca20c",
-		name: "Taco",
-		ratings: 3,
-		price: "9",
-	},
-	{
-		id: 63,
-		itemId: "taco01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Ftaco5.png?alt=media&token=910f236f-2149-46cf-9b74-7d1ee0323c93",
-		name: "Taco",
-		ratings: 3,
-		price: "9",
-	},
-	{
-		id: 64,
-		itemId: "taco01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Ftaco6.png?alt=media&token=91214a30-cad0-434d-9339-3c658150ccab",
-		name: "Taco",
-		ratings: 3,
-		price: "9",
-	},
-	{
-		id: 65,
-		itemId: "snack01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fsnack1.png?alt=media&token=d670617e-4bfc-4693-aa8c-211db9f9d706",
-		name: "Snack",
-		ratings: 4,
-		price: "9",
-	},
-	{
-		id: 66,
-		itemId: "snack01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fsnack2.png?alt=media&token=55d7fc65-869b-4d8b-83b3-be0158474413",
-		name: "Snack",
-		ratings: 4,
-		price: "9",
-	},
-	{
-		id: 67,
-		itemId: "snack01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fsnack3.png?alt=media&token=76b488a6-be44-4a3e-a29e-c34ac17f2e2c",
-		name: "Snack",
-		ratings: 4,
-		price: "9",
-	},
-	{
-		id: 68,
-		itemId: "snack01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fsnack4.png?alt=media&token=1efcd8c0-098b-44a5-8113-da225e7358ce",
-		name: "Snack",
-		ratings: 4,
-		price: "9",
-	},
-	{
-		id: 69,
-		itemId: "snack01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fsnack5.png?alt=media&token=80cece50-38cd-489f-a73f-edd740fba4ab",
-		name: "Snack",
-		ratings: 4,
-		price: "9",
-	},
-	{
-		id: 70,
-		itemId: "snack01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fsnack6.png?alt=media&token=a36cb2c5-72d1-4364-9817-3a8eb8c244b6",
-		name: "Snack",
-		ratings: 4,
-		price: "9",
-	},
-	{
-		id: 71,
-		itemId: "drink01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdrink1.png?alt=media&token=e9cc5e4e-2194-4766-9d5a-5e01657927b7",
-		name: "Soft Drinks",
-		ratings: 4,
-		price: "9",
-	},
-	{
-		id: 72,
-		itemId: "drink01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdrink2.png?alt=media&token=872c5ddd-23ae-4f1e-b1dd-18d375240ba6",
-		name: "Soft Drinks",
-		ratings: 4,
-		price: "7.5",
-	},
-	{
-		id: 73,
-		itemId: "drink01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdrink3.png?alt=media&token=c0efdccf-149a-4063-9658-00ae6c7c69dc",
-		name: "Soft Drinks",
-		ratings: 4,
-		price: "7.5",
-	},
-	{
-		id: 74,
-		itemId: "drink01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdrink4.png?alt=media&token=eaaa8826-7fc0-499f-9eaf-8369ff99c112",
-		name: "Soft Drinks",
-		ratings: 4,
-		price: "7.5",
-	},
-	{
-		id: 75,
-		itemId: "drink01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdrink5.png?alt=media&token=eecf1eee-73f5-4705-a7ce-9ccc363f4f47",
-		name: "Soft Drinks",
-		ratings: 4,
-		price: "7.5",
-	},
-	{
-		id: 76,
-		itemId: "drink01",
-		imgSrc:
-			"https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdrink6.png?alt=media&token=86c4ba2c-0db7-47f0-b8cf-fed24db6c1ae",
-		name: "Soft Drinks",
-		ratings: 4,
-		price: "7.5",
+		price: "450",
 	},
 ];
 
 export { MenuItems, Items };
+
